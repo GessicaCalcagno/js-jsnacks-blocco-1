e1662c6281c54b1c7ae3d4a1dd9f4e5c915247fc
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-04-02 20:12:57 +0200

    Add: jsnack7

---
+++ jsnack7/script.js
@@ -0,0 +1,19 @@
+//--------------------- ESERCIZIO 7-------------------------------------
+//Stampa le potenze di 2 fino a 1000
+//----------------------------------------------------------
+
+// TEST
+//alert('ciao')
+
+//************************************************ */
+// Lo usiamo come controllo alla fine se c'è qualche problema di sintassi! COMMENTALO SE ANCORA STAI LAVORANDO SUL CODICE
+//"use strict";
+//**************************************************** */
+
+// Dichiaro la variabile
+let potenza = 1;
+
+// Calcolo e stampo tutte le potenze di 2 fino a 1000 in console
+for (let potenza = 1; potenza <= 1000; potenza *= 2) {
+    console.log(potenza);
+}
