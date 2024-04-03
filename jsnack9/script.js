c6c98f86d52d8900a8e4a87b29915ee982a3d089
GessicaCalcagno <gessicacristinacalcagno@gmail.com>
2024-04-03 18:23:14 +0200

    Add: jsnack9

---
+++ jsnack9/script.js
@@ -0,0 +1,21 @@
+//--------------------- ESERCIZIO 9-------------------------------------
+//Calcola la somma e la media dei primi 10 numeri.
+//----------------------------------------------------------
+
+// TEST
+//alert('ciao')
+
+//************************************************ */
+// Lo usiamo come controllo alla fine se c'è qualche problema di sintassi! COMMENTALO SE ANCORA STAI LAVORANDO SUL CODICE
+"use strict";
+//**************************************************** */
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
+    console.log("La somma è:", i, sum);
+}
+
+let average = sum / 10;
+console.log("La media è:" + average);
